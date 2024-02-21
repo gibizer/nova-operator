f1592726971a1c516a3266250f4b1511239131a3
Kamil Sambor <kamil.sambor@gmail.com>
2024-02-21 11:06:54 +0100

    [AddMemcachedInstance] Add missing finalizer and watches for memcached

--- test/functional/nova_controller_test.go
+++ test/functional/nova_controller_test.go
@@ -1001,11 +1001,13 @@ var _ = Describe("Nova controller without memcached", func() {
 			condition.ServiceAccountReadyCondition,
 			corev1.ConditionTrue,
 		)
-		th.ExpectCondition(
+		th.ExpectConditionWithDetails(
 			novaNames.NovaName,
 			ConditionGetterFunc(NovaConditionGetter),
 			condition.MemcachedReadyCondition,
 			corev1.ConditionFalse,
+			condition.RequestedReason,
+			" Memcached instance has not been provisioned",
 		)
 	})
 })
